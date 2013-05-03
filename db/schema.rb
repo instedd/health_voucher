bb6075c64241f05c4c9f6c744776abf612d8ea56
Gustavo Giráldez <gustavo.giraldez@gmail.com>
2013-05-03 11:17:30 -0300

    Add admin flag to users table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130503140950) do
+ActiveRecord::Schema.define(:version => 20130503141448) do
 
   create_table "authorizations", :force => true do |t|
     t.integer  "card_id"
@@ -167,8 +167,8 @@ ActiveRecord::Schema.define(:version => 20130503140950) do
   add_index "transactions", ["voucher_id"], :name => "index_transactions_on_voucher_id"
 
   create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "email",                  :default => "",    :null => false
+    t.string   "encrypted_password",     :default => "",    :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -177,8 +177,9 @@ ActiveRecord::Schema.define(:version => 20130503140950) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at",                                :null => false
+    t.datetime "updated_at",                                :null => false
+    t.boolean  "admin",                  :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
