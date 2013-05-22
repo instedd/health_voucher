3a4102cf9c00fa42615a1b95375e346341f8d5c6
Gustavo Giráldez <gustavo.giraldez@gmail.com>
2013-05-22 14:17:59 -0300

    Remove Nuntium API gem (wasn't really needed in the first place)

--- config/initializers/nuntium.rb
+++
@@ -1,8 +0,0 @@
-class	Nuntium
-	
-	def self.from_config
-		config = EVoucher::Application.config
-		Nuntium.new config.nuntium_host, config.nuntium_account, config.nuntium_app, config.nuntium_app_passwd
-	end
-	
-end
