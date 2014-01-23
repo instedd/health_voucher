1c12e28bdf9b485691a5b6d625b5188148fd06cb
Gustavo Giráldez <ggiraldez@manas.com.ar>
2014-01-23 11:04:34 -0300

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -110,25 +110,6 @@ ActiveRecord::Schema.define(:version => 20130530165145) do
 
   add_index "mentors", ["site_id"], :name => "index_mentors_on_site_id"
 
-  create_table "models", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.string   "User"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
-  end
-
-  add_index "models", ["email"], :name => "index_models_on_email", :unique => true
-  add_index "models", ["reset_password_token"], :name => "index_models_on_reset_password_token", :unique => true
-
   create_table "patients", :force => true do |t|
     t.string   "agep_id"
     t.integer  "mentor_id"
