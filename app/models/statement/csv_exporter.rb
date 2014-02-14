243d76a3860708a5c3dc1bb09bd12745adf16b6b
Gustavo Giráldez <ggiraldez@manas.com.ar>
2014-02-14 12:32:30 -0300

    Add provider name and service description to statement page and CSV export.

--- app/models/statement/csv_exporter.rb
+++ app/models/statement/csv_exporter.rb
@@ -6,10 +6,10 @@ class Statement::CsvExporter < CsvExporter
   end
 
   def export
-    headers "# Txn ID", "Date", "Provider", "Service", "AGEP ID", "Voucher", "Amount"
+    headers "# Txn ID", "Date", "Provider Code", "Provider Name", "Service Code", "Service", "AGEP ID", "Voucher", "Amount"
     generate do |csv|
       @stmt.transactions.for_listing.each do |txn|
-        csv << [txn.id, txn.created_at, txn.provider.code, txn.service.code, txn.patient.agep_id, card_serial_number(txn.card), txn.amount]
+        csv << [txn.id, txn.created_at, txn.provider.code, txn.provider.name, txn.service.code, txn.service.description, txn.patient.agep_id, card_serial_number(txn.card), txn.amount]
       end
     end
   end
