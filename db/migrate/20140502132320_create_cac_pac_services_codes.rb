effe6cd8b1d75e536c1352eb3ca1070855ea2538
Gustavo Giráldez <ggiraldez@manas.com.ar>
2014-05-02 16:56:20 -0300

    Replaced some special characters by their ASCII equivalent

--- db/migrate/20140502132320_create_cac_pac_services_codes.rb
+++ db/migrate/20140502132320_create_cac_pac_services_codes.rb
@@ -1,15 +1,15 @@
 class CreateCacPacServicesCodes < ActiveRecord::Migration
   def up
-    connection.execute "UPDATE services SET description = 'CAC – Medical Safe abortion (TOP) - mifepristone and misoprostol (MA)', " +
+    connection.execute "UPDATE services SET description = 'CAC - Medical Safe abortion (TOP) - mifepristone and misoprostol (MA)', " +
       "short_description = 'CAC Medical' WHERE code = '71'"
-    connection.execute "UPDATE services SET description = 'CAC – Surgical Safe abortion (TOP) - vacuum aspiration (MVA)/surgical', " +
+    connection.execute "UPDATE services SET description = 'CAC - Surgical Safe abortion (TOP) - vacuum aspiration (MVA)/surgical', " +
       "short_description = 'CAC Surgical' WHERE code = '72'"
     connection.execute "INSERT INTO services " +
       "(service_type, description, short_description, code, created_at, updated_at) VALUES " +
-      "('primary', 'PAC – Medical Incomplete abortion (PAC) - misoprostol (MPAC)', 'PAC Medical', '73', NOW(), NOW())"
+      "('primary', 'PAC - Medical Incomplete abortion (PAC) - misoprostol (MPAC)', 'PAC Medical', '73', NOW(), NOW())"
     connection.execute "INSERT INTO services " +
       "(service_type, description, short_description, code, created_at, updated_at) VALUES " +
-      "('primary', 'PAC – Surgical Incomplete abortion (PAC) - vacuum aspiration (MVA)/surgical', 'PAC Surgical', '74', NOW(), NOW())"
+      "('primary', 'PAC - Surgical Incomplete abortion (PAC) - vacuum aspiration (MVA)/surgical', 'PAC Surgical', '74', NOW(), NOW())"
   end
 
   def down
