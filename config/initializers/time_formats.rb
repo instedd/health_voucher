666635cbc1a45d99643183d9162ad0e400ded12e
Gustavo Giráldez <ggiraldez@manas.com.ar>
2014-10-21 14:38:39 -0300

    Add Distribution sheets to exported statements to Excel

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,4 +1,5 @@
 Time::DATE_FORMATS[:date] = "%m/%d/%Y"
 Time::DATE_FORMATS[:file_date] = "%Y%m%d"
 Time::DATE_FORMATS[:transaction] = "%m/%d/%Y %H:%M:%S"
+Date::DATE_FORMATS[:statement_month] = "%B %Y"
 
